233bae0c30db5b922caeed1850145289f65f6d51
Andrian-Aff <affear.123@gmail.com>
2021-07-06 22:45:18 +0300

    finish second task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsContainerEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(option => {
+  const listEL = document.createElement('li');
+  listEL.textContent = option;
+  return listEL;
+});
+
+ingredientsContainerEl.append(...elements);
