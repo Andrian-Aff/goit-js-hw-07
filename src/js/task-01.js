7afb519357bad5f31c03b13802982a29c871a36c
Andrian-Aff <affear.123@gmail.com>
2021-07-13 11:35:24 +0300

    corrected first task

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -3,6 +3,6 @@ console.dir(categoriesEl);
 const message = `В списке ${categoriesEl.length} категории`;
 console.log(message);
 categoriesEl.forEach((el) => {
-  console.log("Категория", el.querySelector(".item h2").textContent);
-  console.log("Количество", el.querySelectorAll("ul li").length);
+  console.log(`Категория:  ${el.firstElementChild.textContent}`);
+  console.log(`Количество элементов: ${el.lastElementChild.children.length}`);
 });
